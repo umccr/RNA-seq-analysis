081b60525eabe4fbf560b5753b661e00b95c83ff
JMarzec <jacek.d.marzec@gmail.com>
2018-07-18 12:27:45 +1000

    Change input parameter naming to follow convention in other scripts

--- readcount-analysis/combineExprData.R
+++ readcount-analysis/combineExprData.R
@@ -14,9 +14,9 @@
 #
 #	 Description: Pipeline combining gene-by-sample expression matrices from different datasets. It requires manually prepared dataset file with four columns ("Dataset_name", "Expression_matrix", "Target_file" and "Outliers_file") to define names of the datasets to be merged, the location of the correspoding expression matrices and target files, samples's names for the merged matrix, as well as to the files listing outlier samples to be removed before combining the data. Note, only genes intersection across all datasets expression matrices will be reported in the combined expression matrix. The pipeline is based on recommendaitons from RNAseq123 R package (https://master.bioconductor.org/packages/release/workflows/vignettes/RNAseq123/inst/doc/limmaWorkflow.html).
 #
-#	 Command line use example: Rscript  combineExprData.R --project_dir /Combined_data --datasets Datasets_list.txt
+#	 Command line use example: Rscript  combineExprData.R --projectDir /Combined_data --datasets Datasets_list.txt
 #
-#   project_dir:  Project directory. This is where the datasets file is expeced and where combined expression matrix and accompanying file will be saved
+#   projectDir:   Project directory. This is where the datasets file is expeced and where combined expression matrix and accompanying file will be saved
 #   datasets:     Name of the datasets file listing info about datasets to combine. It expects to have four columns: (1) Dataset_name, (2) Expression_matrix, (3) Target_file and (4) Outliers_file
 #
 ################################################################################
@@ -38,7 +38,7 @@ suppressMessages(library(optparse))
 #===============================================================================
 
 option_list = list(
-  make_option(c("-p", "--project_dir"), action="store", default=NA, type='character',
+  make_option(c("-p", "--projectDir"), action="store", default=NA, type='character',
               help="Project directory"),
   make_option(c("-d", "--datasets"), action="store", default=NA, type='character',
               help="Name of the datasets file listing info about datasets to combine")
@@ -47,13 +47,13 @@ option_list = list(
 opt = parse_args(OptionParser(option_list=option_list))
 
 ##### Read in argument from command line and check if all were provide by the user
-if ( is.na(opt$project_dir) || is.na(opt$datasets) ) {
+if ( is.na(opt$projectDir) || is.na(opt$datasets) ) {
   
   cat("\nPlease type in required arguments!\n\n")
-  cat("\ncommand example:\n\ncombineExprData.R --project_dir /Combined_data --datasets Datasets_list.txt\n\n")
+  cat("\ncommand example:\n\ncombineExprData.R --projectDir /Combined_data --datasets Datasets_list.txt\n\n")
   
   q()
 }
 
 ##### Pass the user-defined argumentas to the SVbezierPlot R markdown script and run the analysis
-rmarkdown::render(input = "combineExprData.Rmd", output_file = paste0(opt$datasets, ".combineExprData.html"), output_dir = opt$project_dir, params = list(projectDir = opt$project_dir, datasetsFile = opt$datasets))
+rmarkdown::render(input = "combineExprData.Rmd", output_file = paste0(opt$datasets, ".combineExprData.html"), output_dir = opt$projectDir, params = list(projectDir = opt$projectDir, datasetsFile = opt$datasets))
