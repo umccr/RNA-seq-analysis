a2107fc50e17e9d7dfaa0e9b5b6e5dc9ca23552e
JMarzec <jacek.d.marzec@gmail.com>
2019-03-07 11:10:35 +1100

    Retrieve changes performed in the batch effect ajdustement branch

--- readcount-analysis/combineExprData.R
+++ readcount-analysis/combineExprData.R
@@ -12,12 +12,18 @@
 
 ################################################################################
 #
-#	 Description: Pipeline combining gene-by-sample expression matrices from different datasets. It requires manually prepared dataset file with four columns ("Dataset_name", "Expression_matrix", "Target_file" and "Outliers_file") to define names of the datasets to be merged, the location of the correspoding expression matrices and target files, samples's names for the merged matrix, as well as to the files listing outlier samples to be removed before combining the data. Note, only genes intersection across all datasets expression matrices will be reported in the combined expression matrix. The pipeline is based on recommendaitons from RNAseq123 R package (https://master.bioconductor.org/packages/release/workflows/vignettes/RNAseq123/inst/doc/limmaWorkflow.html).
+#	 Description: Pipeline transforming and normalising expression matrix from multiple samples. It requires accompanying sample annotation file with four columns: (1) "Sample_name", (2) "File_name" (may be balnk), (3) "Target" and (4) "Replicates" (may be balnk). The pipeline is based on recommendaitons from RNAseq123 R package (https://master.bioconductor.org/packages/release/workflows/vignettes/RNAseq123/inst/doc/limmaWorkflow.html).
 #
-#	 Command line use example: Rscript  combineExprData.R --projectDir ./Combined_data --datasets Datasets_list.txt
+#	 Command line use example: Rscript  combineExprData.R --exprDir /Combined_data --exprFile CUP.counts.matrix.txt --annotFile CUP_Target.txt --transform CPM --norm TMM --filter TRUE --log TRUE --results_name CUP
 #
-#   projectDir:   Project directory. This is where the datasets file is expeced and where combined expression matrix and accompanying file will be saved
-#   datasets:     Name of the datasets file listing info about datasets to combine. It expects to have four columns: (1) Dataset_name, (2) Expression_matrix, (3) Target_file and (4) Outliers_file
+#   exprDir:      Directory with expression data. This is where the combined expression matrix and accompanying files will be saved
+#   exprFile:     File with expression data (read counts)
+#   annotFile:    Samples annotation file with four columns: (1) "Sample_name", (2) "File_name" (may be balnk), (3) "Target" and (4) "Replicates" (may be balnk)
+#   transform:    Transformation method to be used when converting read counts. Available options are: "CPM" (defualt) and "TPM"
+#   norm:         Normalisation method. Currently, "TMM","TMMwzp", "RLE" and "upperquartile" methods are available for CPM-transformed data and "quantile" normalisation is used for TPM-transformed data
+#   filter:       Filtering out low expressed genes. Available options are: "TRUE" (defualt) and "FALSE"
+#   log:          Log (base 2) transform data before normalisation. Available options are: "TRUE" (defualt) and "FALSE"
+#   results_name: Desired core name for the results folder
 #
 ################################################################################
 
@@ -38,22 +44,67 @@ suppressMessages(library(optparse))
 #===============================================================================
 
 option_list = list(
-  make_option(c("-p", "--projectDir"), action="store", default=NA, type='character',
-              help="Project directory"),
-  make_option(c("-d", "--datasets"), action="store", default=NA, type='character',
-              help="Name of the datasets file listing info about datasets to combine")
+  make_option(c("-d", "--exprDir"), action="store", default=NA, type='character',
+              help="Directory with expression data"),
+  make_option(c("-e", "--exprFile"), action="store", default=NA, type='character',
+              help="File with expression data (read counts)"),
+  make_option(c("-a", "--annotFile"), action="store", default=NA, type='character',
+              help="Samples annotation file"),
+  make_option(c("-t", "--transform"), action="store", default=NA, type='character',
+              help="Transformation method to be used when converting read counts"),
+  make_option(c("-f", "--filter"), action="store", default=NA, type='character',
+              help="Normalisation method"),
+  make_option(c("-n", "--norm"), action="store", default=NA, type='character',
+              help="Filtering out low expressed genes"),
+  make_option(c("-l", "--log"), action="store", default=NA, type='character',
+              help="Log (base 2) transform data before normalisation"),
+  make_option(c("-r", "--results_name"), action="store", default=NA, type='character',
+              help="Log (base 2) transform data before normalisation")
 )
 
 opt = parse_args(OptionParser(option_list=option_list))
 
+opt$filter <- as.logical(opt$filter)
+opt$log <- as.logical(opt$log)
+
 ##### Read in argument from command line and check if all were provide by the user
-if ( is.na(opt$projectDir) || is.na(opt$datasets) ) {
+if ( is.na(opt$exprDir) || is.na(opt$exprFile) || is.na(opt$annotFile) || is.na(opt$filter) || is.na(opt$log) ) {
   
   cat("\nPlease type in required arguments!\n\n")
-  cat("\ncommand example:\n\ncombineExprData.R --projectDir /Combined_data --datasets Datasets_list.txt\n\n")
+  cat("\ncommand example:\n\nRscript  combineExprData.R --exprFile /Combined_data --exprFile CUP.counts.matrix.txt --annotFile CUP_Target.txt --transform CPM --norm TMM --filter TRUE --log TRUE\n\n")
   
   q()
 }
 
+##### Make sure that TMM, TMMwzp, RLE or upperquartile normalisation is used for CPM-tansformed data and quantile normalisation is used for TPM-tansformed data
+if ( opt$transform == "TPM" && opt$norm == "TMM" ) {
+  
+  cat(paste0("\nOnly TPM normalisation is not available for TPM-tansformed data!\n\nQuantile normalisation will be performed for ", opt$transform, "-tansformed data.\n\n"))
+  
+  opt$norm <- "quantile"
+  
+} else if ( opt$transform == "CPM" && opt$norm == "quantile" ) {
+  
+  cat(paste0("\nQuantile normalisation is available only for TPM-tansformed data! \"TMM\", \"TMMwzp\", \"RLE\" and \"upperquartile\" methods are available for ", opt$transform, "-tansformed data.\n\n"))
+  
+  q()
+  
+} else if ( opt$transform == "CPM" &&  opt$norm != "TMM" && opt$norm != "TMMwzp" && opt$norm != "RLE" && opt$norm != "upperquartile" ) {
+  
+  cat(paste0("\nWrong normalisation method was selected! \"TMM\", \"TMMwzp\", \"RLE\" and \"upperquartile\" methods are available for ", opt$transform, "-tansformed data.\n\n"))
+  
+  q()
+}
+
+##### Check if the named of the results folder is defined
+if ( !is.na(opt$results_name) ) {
+  
+  opt$results_name <- paste0(opt$results_name, "_", opt$transform, "_", opt$norm, ".html")
+  
+} else {
+  opt$results_name <- paste0(opt$exprFile, "_", opt$transform, "_", opt$norm, ".html")
+}
+
+
 ##### Pass the user-defined argumentas to the SVbezierPlot R markdown script and run the analysis
-rmarkdown::render(input = "combineExprData.Rmd", output_file = paste0(opt$datasets, ".combineExprData.html"), output_dir = opt$projectDir, params = list(projectDir = opt$projectDir, datasetsFile = opt$datasets))
+rmarkdown::render(input = "combineExprData.Rmd", output_file = opt$results_name, output_dir = opt$exprDir, params = list(exprDir = opt$exprDir, exprFile = opt$exprFile, annotFile = opt$annotFile, transform = opt$transform, filter = opt$filter, norm = opt$norm, log = opt$log, results_name = opt$results_name))
--- readcount-analysis/mergeCounts2Matrix.R
+++ readcount-analysis/mergeCounts2Matrix.R
@@ -12,9 +12,9 @@
 
 ################################################################################
 #
-#   Description: Script merging multiple per-sample expression files in user-defined directory into a matrix. It requires manually prepared target file with four columns (1 = Sample_name, 2 = File_name, 3 = Target and 4 = Replicates) to indicate the files to be merged, samples's names for the merged matrix, samples's phenotype for downstream analyses and inictation of technical replicates. Note, only genes intersection across all per-sample files will be reported in the merged matrix.
+#   Description: Script merging multiple per-sample expression files in user-defined directory into a matrix. It requires manually prepared target file with four columns (1 = Sample_name, 2 = File_name, 3 = Target and 4 = Replicates) to indicate the files to be merged, samples's names for the merged matrix, samples's phenotype for downstream analyses and inictation of technical replicates. Note, only genes intersection across all per-sample files will be reported in the merged matrix. 
 #
-#   Command line use example: Rscript mergeCounts2Matrix.R --projectDir ./Combined_data --target ./TCGA-PAAD/TCGA_PAAD_Target.txt --inDir ./TCGA-PAAD --outFile TCGA-PAAD
+#   Command line use example: Rscript mergeCounts2Matrix.R --projectDir /Combined_data --target /TCGA_PAAD_Target.txt --inDir /TCGA-PAAD --outFile TCGA-PAAD
 #
 #   projectDir:   Project directory. This is where the target file is expected and where the merged matrix will be saved
 #   target:       Name of the target file. It expects to have four columns: (1) Sample_name, (2) File_name, (3) Target and (4) Replicates
@@ -39,15 +39,15 @@ graphics.off()
 
 ##### Prepare object to write into a file
 prepare2write <- function (x) {
-
-    x2write <- cbind(rownames(x), x)
-    colnames(x2write) <- c("",colnames(x))
-    return(x2write)
+  
+  x2write <- cbind(rownames(x), x)
+  colnames(x2write) <- c("",colnames(x))
+  return(x2write)
 }
 
 ##### Prepare gene data matrix to write into a file
 geneMatrix2write <- function (x) {
-
+  
   x2write <- cbind(rownames(x), x)
   colnames(x2write) <- c("Gene",colnames(x))
   return(x2write)
@@ -59,7 +59,7 @@ geneMatrix2write <- function (x) {
 #===============================================================================
 
 suppressMessages(library(optparse))
-suppressMessages(library(DESeq2))
+
 
 #===============================================================================
 #    Catching the arguments
@@ -89,20 +89,19 @@ outFile <- opt$outFile
 #===============================================================================
 
 ##### Read in the target file
-targets <- read.table(targetFile, header=TRUE, sep="\t", row.names=1)
+targets <- read.table(paste(projectDir,targetFile, sep="/"), header=TRUE, sep="\t", row.names=1)
 
 ##### Make syntactically valid names
 rownames(targets) <- make.names(rownames(targets))
 
-##### Record the current directory and change to directory with per-sample expression files
-currDir <- getwd()
+##### Change to directory with per-sample expression files
 setwd(inFileDir)
 
 ##### Get the list of all files in the user-defined directory
 file_list <- list.files()
 
 ##### Check if any of the files listed in target file are missing. Write them into a file
-file_list.missing <- file_list[ targets$File_name %!in% file_list ]
+file_list.missing <- targets$File_name[ targets$File_name %!in% file_list ]
 
 if ( length(file_list.missing) > 0 ) {
   write.table(prepare2write(file_list.missing), file = paste0(projectDir, "/", outFile, ".mergeCounts2Matrix.missing_files.txt" ), sep="\t", quote=FALSE, row.names=TRUE, append = FALSE )
@@ -116,59 +115,41 @@ gene_list <- NULL
 
 ##### Loop through the per-sample expression files and merge them into a matrix
 for (file in file_list){
-
-    ##### Create merged dataset variable if it doesn't exist yet
-    if (!exists("dataset")){
-        dataset <- as.data.frame( read.table(file, header=FALSE, sep="\t", row.names=NULL) )
-        colnames(dataset) <- c( "Gene", rownames(targets)[targets$File_name == file] )
-
-        ##### list genes present in individal files
-        gene_list <- as.vector(dataset$Gene)
-
-    ##### Add data for the remaining samples
-    } else if (exists("dataset")) {
-        sample <-as.data.frame( read.table(file, header=FALSE, sep="\t", row.names=NULL) )
-        colnames(sample) <- c( "Gene", rownames(targets)[targets$File_name == file] )
-
-        ##### list genes present in individal files
-        gene_list <- c( gene_list, as.vector(sample$Gene) )
-
-        ##### Merge the expression data and make sure that the genes order is the same
-        dataset <- merge( dataset, sample, by="Gene", all = FALSE, sort= TRUE)
-
-        ##### Remove per-sample data for merged samples to free some memory
-        rm(sample)
-    }
+  
+  ##### Create merged dataset variable if it doesn't exist yet
+  if (!exists("dataset")){
+    dataset <- as.data.frame( read.table(file, header=FALSE, sep="\t", row.names=NULL) )
+    colnames(dataset) <- c( "Gene", rownames(targets)[targets$File_name == file] )
+    
+    ##### list genes present in individal files
+    gene_list <- as.vector(dataset$Gene)
+    
+    ##### Add data for the remaining samples   
+  } else if (exists("dataset")) {
+    sample <-as.data.frame( read.table(file, header=FALSE, sep="\t", row.names=NULL) )
+    colnames(sample) <- c( "Gene", rownames(targets)[targets$File_name == file] )
+    
+    ##### list genes present in individal files
+    gene_list <- c( gene_list, as.vector(sample$Gene) )
+    
+    ##### Merge the expression data and make sure that the genes order is the same
+    dataset <- merge( dataset, sample, by="Gene", all = FALSE, sort= TRUE)
+    
+    ##### Remove per-sample data for merged samples to free some memory
+    rm(sample)
+  }
 }
 
 ##### Use gene IDs as rownames
 rownames(dataset) <- dataset$Gene
 dataset <- dataset[, -1]
 
-#### Batch-effect correction using DESeq2
-dds <- DESeqDataSetFromMatrix(countData = dataset, colData = targets, design = ~Target)
-
 ##### Make syntactically valid names
 colnames(dataset) <- make.names(colnames(dataset))
-
-##### Make sure that the target file contains info only about samples present in the data matrix
-targets <- targets[ rownames(targets) %in% colnames(dataset),  ]
-
-##### Make sure that the samples order in the data matrix is the same as in the target file 
-dataset <- dataset[ , rownames(targets) ]
-
-####
-dds <- DESeqDataSet(countData = dataset, coldata = targets, design = ~Target)
-
-dds <- DESeq(dds)
-
 
 ##### Identify genes that were not present across all per-sampel files and were ommited in the merged matrix
 gene_list <- unique(gene_list)
 gene_list.missing <- gene_list[ gene_list %!in% rownames(dataset) ]
-
-##### Go back to the original directory in case user refers the input or output directories using relative path
-setwd(currDir)
 
 ##### Write list of missing genes into a file
 if ( length(gene_list.missing) > 0 ) {
